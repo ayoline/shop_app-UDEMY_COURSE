9c56466ae9218d1e05d9bd720434680bf44b8d9a
Anderson P Soares <ayo@outlook.com.br>
2023-04-06 17:02:03 -0300

    add shopping cart data

--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -5,12 +5,14 @@ import '../providers/products.dart';
 import './products_item.dart';
 
 class ProductsGrid extends StatelessWidget {
-  const ProductsGrid({super.key});
+  final bool showFavs;
+
+  const ProductsGrid(this.showFavs, {super.key});
 
   @override
   Widget build(BuildContext context) {
     final productsData = Provider.of<Products>(context);
-    final products = productsData.items;
+    final products = showFavs ? productsData.favoriteItems : productsData.items;
 
     return GridView.builder(
       padding: const EdgeInsets.all(10),
@@ -23,7 +25,7 @@ class ProductsGrid extends StatelessWidget {
       ),
       itemBuilder: (ctx, i) => ChangeNotifierProvider.value(
         value: products[i],
-        child: ProductItem(
+        child: const ProductItem(
             // products[i].id,
             // products[i].title,
             // products[i].imageUrl,
