a0e74c9dbd192df80bb03dfec92c8270c10139e0
Anderson P Soares <ayo@outlook.com.br>
2023-04-06 18:30:41 -0300

    connect the cart provider

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,6 +1,11 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: constant_identifier_names
 
+import 'package:flutter/material.dart' hide Badge;
+import 'package:provider/provider.dart';
+
+import '../providers/cart.dart';
 import '../widgets/products_grid.dart';
+import '../widgets/badge.dart';
 
 enum FilterOptions {
   Favorites,
@@ -35,10 +40,26 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
             },
             icon: const Icon(Icons.more_vert),
             itemBuilder: (_) => [
-              const PopupMenuItem(value: FilterOptions.Favorites, child: Text('Only Favorites')),
-              const PopupMenuItem(value: FilterOptions.All, child: Text('Show all')),
+              const PopupMenuItem(
+                value: FilterOptions.Favorites,
+                child: Text('Only Favorites'),
+              ),
+              const PopupMenuItem(
+                value: FilterOptions.All,
+                child: Text('Show all'),
+              ),
             ],
-          )
+          ),
+          Consumer<Cart>(
+            builder: (_, cart, ch) => Badge(
+              value: cart.itemCount.toString(),
+              child: ch!,
+            ),
+            child: IconButton(
+              icon: const Icon(Icons.shopping_cart),
+              onPressed: () {},
+            ),
+          ),
         ],
       ),
       body: ProductsGrid(_showOnlyFavorites),
