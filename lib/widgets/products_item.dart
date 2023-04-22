f3e8d8cbc62d33973324b742f0e7a94b4802701d
Anderson P Soares <ayo@outlook.com.br>
2023-04-22 20:09:13 -0300

    use ScaffoldMessenger to Undoing 'Add to cart' actions

--- lib/widgets/products_item.dart
+++ lib/widgets/products_item.dart
@@ -41,10 +41,21 @@ class ProductItem extends StatelessWidget {
             icon: const Icon(Icons.shopping_cart),
             color: Theme.of(context).colorScheme.secondary,
             onPressed: () {
-              cart.addItem(
-                product.id,
-                product.price,
-                product.title,
+              cart.addItem(product.id, product.price, product.title);
+              ScaffoldMessenger.of(context).hideCurrentSnackBar();
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: const Text(
+                    'Added item to Cart!',
+                  ),
+                  duration: const Duration(seconds: 2),
+                  action: SnackBarAction(
+                    label: 'UNDO',
+                    onPressed: () {
+                      cart.removeSingleItem(product.id);
+                    },
+                  ),
+                ),
               );
             },
           ),
