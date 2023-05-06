278a23c4a4bf8e55c566afc0ab174251b6c0c14d
Anderson P Soares <ayo@outlook.com.br>
2023-05-05 22:20:37 -0300

    allow users to delete product

--- lib/widgets/user_product_item.dart
+++ lib/widgets/user_product_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import '../providers/products.dart';
 import '../screens/edit_product_screen.dart';
 
 class UserProductItem extends StatelessWidget {
@@ -36,7 +38,9 @@ class UserProductItem extends StatelessWidget {
               },
             ),
             IconButton(
-              onPressed: () {},
+              onPressed: () {
+                Provider.of<Products>(context, listen: false).deleteProduct(id);
+              },
               icon: const Icon(Icons.delete),
               color: Theme.of(context).colorScheme.error,
             )
